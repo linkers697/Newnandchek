8917168205736c599c3e5edf8b54c686dc739f95
linkers697 <music3fhhgyhfgh@gmail.com>
2025-09-27 21:06:17 +0530

    Update Youtube.py

--- ShrutiMusic/platforms/Youtube.py
+++ ShrutiMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from AviaxMusic.utils.database import is_on_off
-from AviaxMusic.utils.formatters import time_to_seconds
+from ShrutiMusic.utils.database import is_on_off
+from ShrutiMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
